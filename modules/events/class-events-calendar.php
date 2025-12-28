43c1131adfdf67e9b67adfc0a8f320f254f14989
Socialforger <socialforger@gmail.com>
2025-12-28 22:05:16 +0100

    Create class-events-calendar.php

---
+++ modules/events/class-events-calendar.php
@@ -0,0 +1,13 @@
+<?php
+namespace Social_Market\Modules\Events;
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class Events_Calendar {
+
+    public static function get_events( $args = array() ) {
+        return array(); // Placeholder
+    }
+}
