dd7ae99db9514e6ab2e6ee1f0aa9a4997def264e
Socialforger <socialforger@gmail.com>
2025-12-28 22:06:27 +0100

    Create class-events-notifications.php

---
+++ modules/events/class-events-notifications.php
@@ -0,0 +1,13 @@
+<?php
+namespace Social_Market\Modules\Events;
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class Events_Notifications {
+
+    public static function notify_users( $event_id ) {
+        // Placeholder
+    }
+}
