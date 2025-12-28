04fd1edada7967548d05c46f13b3cc949cebc550
Socialforger <socialforger@gmail.com>
2025-12-28 22:35:39 +0100

    Create single-gas.php

---
+++ templates/gas/single-gas.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template: Singolo GAS
+ * Override path: yourtheme/social-market/gas/single-gas.php
+ */
+
+get_header();
+?>
+
+<div class="sm-container sm-single-gas">
+
+    <h1 class="sm-title"><?php the_title(); ?></h1>
+
+    <div class="sm-content">
+        <?php the_content(); ?>
+    </div>
+
+    <div class="sm-meta sm-gas-meta">
+        <?php
+        // Qui in futuro potremo aggiungere:
+        // - membri del GAS
+        // - sessioni di ritiro
+        // - prossima consegna
+        // - calendario eventi
+        ?>
+    </div>
+
+</div>
+
+<?php
+get_footer();
