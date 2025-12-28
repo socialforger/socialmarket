e27af2fe49349d9a5596490dbffe715e12614590
Socialforger <socialforger@gmail.com>
2025-12-28 22:02:40 +0100

    Create class-membership-gas.php

---
+++ modules/membership/class-membership-gas.php
@@ -0,0 +1,13 @@
+<?php
+namespace Social_Market\Modules\Membership;
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class Membership_GAS {
+
+    public static function get_gas_members( $gas_id ) {
+        return array(); // Placeholder
+    }
+}
