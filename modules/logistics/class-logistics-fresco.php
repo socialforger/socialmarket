221ef808e02b5d7a9dc166e017a35cec2af339e2
Socialforger <socialforger@gmail.com>
2025-12-28 22:04:16 +0100

    Create class-logistics-fresco.php

---
+++ modules/logistics/class-logistics-fresco.php
@@ -0,0 +1,13 @@
+<?php
+namespace Social_Market\Modules\Logistics;
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class Logistics_Fresco {
+
+    public static function is_fresh_product( $product_id ) {
+        return false; // Placeholder
+    }
+}
