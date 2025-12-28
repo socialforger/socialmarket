5912e11fe59c11e42edea064e6fa0164134eb46f
Socialforger <socialforger@gmail.com>
2025-12-28 22:53:30 +0100

    Create calendario-eventi.php

---
+++ templates/partials/calendario-eventi.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Partial: Calendario Eventi
+ *
+ * Percorso nel plugin:
+ *   wp-content/plugins/socialmarket/templates/partials/calendario-eventi.php
+ *
+ * Override nel tema:
+ *   wp-content/themes/tuo-tema/socialmarket/partials/calendario-eventi.php
+ */
+
+?>
+
+<div class="sm-calendario-eventi">
+    <h3><?php _e( 'Calendario Eventi', 'socialmarket' ); ?></h3>
+
+    <p><?php _e( 'Il calendario eventi sarà disponibile prossimamente.', 'socialmarket' ); ?></p>
+
+    <?php
+    // Qui in futuro:
+    // - integrazione con Events_Calendar
+    // - filtri per tipo evento
+    // - vista mensile / settimanale
+    ?>
+</div>
