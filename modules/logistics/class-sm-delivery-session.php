86c8195c1a8f6b9a7857ef38b8cfae200426cd79
Socialforger <socialforger@gmail.com>
2026-06-07 07:15:08 +0200

    Create class-sm-delivery-session.php

---
+++ modules/logistics/class-sm-delivery-session.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class SM_Delivery_Session
+{
+    public static function get_orders(
+        $delivery_id
+    ) {
+
+        return [];
+
+    }
+}
